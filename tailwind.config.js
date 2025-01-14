d8fa317c0570c562ca8d02b9c6dd5555d3ce5e59
BabatundeXBT <babatundezakariyau27@gmail.com>
2025-01-14 15:00:31 +0100

    saving to git

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        verydarc: "	#00494d",
+        darkgrayish: "#5e7a7d",
+        grayishcyan: "	#7f9c9f",
+        lightgrayishcyan: "	#c5e4e7",
+        verylightgrayishcyan: "	#f4fafa",
+        white: "	#ffffff",
+        strongcyan: "	#26c0ab",
+      },
+    },
+  },
+  plugins: [],
+}
+
