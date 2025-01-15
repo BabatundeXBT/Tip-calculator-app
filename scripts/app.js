b0ab0e4f338bea5a76940560ade7c503f1ebae10
BabatundeXBT <babatundezakariyau27@gmail.com>
2025-01-15 10:42:37 +0100

    fix code

--- scripts/app.js
+++ scripts/app.js
@@ -8,20 +8,44 @@ let tip;
 const reset = document.querySelector('.reset');
 
 const clear = () => {
-  bill.value = 0;
-  numberOfPerson.value = 0;
-  tipAmount.textContent = "0.00";
-  totalPerson.textContent = "0.00";
+  bill.value = '';
+  numberOfPerson.value = '';
+  tipAmount.textContent = '0.00';
+  totalPerson.textContent = '0.00';
 };
 reset.addEventListener('click', clear);
 
+// custom button 
+const custom = document.querySelector('.custom');
+custom.addEventListener('keypress', function (e) {
+    // console.log(e);
+    tip = (bill.value * (e.key / 100)) / numberOfPerson.value;
+    tipAmount.textContent = tip.toFixed(2);
+    totalPerson.textContent = (bill.value / numberOfPerson.value + tip).toFixed(
+      2,
+    );
+  });
+  
+// with DRY 
+const btns = document.querySelectorAll('.btn');
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', function () {
+    tip = (bill.value * (Number(btns[i].value))) / numberOfPerson.value;
+    tipAmount.textContent = tip.toFixed(2);
+    totalPerson.textContent = (bill.value / numberOfPerson.value + tip).toFixed(
+      2,
+    );
+  });
+}
+
+// without using DRY 
+/*
 // button containing percentage
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
 const btn4 = document.querySelector('.btn4');
 const btn5 = document.querySelector('.btn5');
-const custom = document.querySelector('.custom');
 
 btn1.addEventListener('click', function () {
   tip = (bill.value * (5 / 100)) / numberOfPerson.value;
@@ -50,10 +74,4 @@ btn5.addEventListener('click', function () {
   tipAmount.textContent = tip.toFixed(2);
   totalPerson.textContent = (bill.value / numberOfPerson.value + tip).toFixed(2);
 });
-
-custom.addEventListener('keypress',function(e){
-    console.log(e)
-    tip = (bill.value * (e.key / 100)) / numberOfPerson.value;
-  tipAmount.textContent = tip.toFixed(2);
-  totalPerson.textContent = (bill.value / numberOfPerson.value + tip).toFixed(2);
-})
+*/
